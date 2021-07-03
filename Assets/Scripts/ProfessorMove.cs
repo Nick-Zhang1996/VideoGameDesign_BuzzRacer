ba6b374716007f043a225f63ffc8e36c72e10483
dayshah <dhyeyhshah@gmail.com>
2021-07-03 15:11:58 -0700

    anotherupdate

---
+++ Assets/Scripts/ProfessorMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProfessorMove : MonoBehaviour
+{
+    public Animator anim;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //anim.transform.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
