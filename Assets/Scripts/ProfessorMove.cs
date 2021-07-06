575f155cdfeb0b6a6eaf63b75e9a097a5d0e3ef3
Shah, Dhyey <dhyeyhshah@gmail.com>
2021-07-06 15:26:51 -0400

    notworking

--- Assets/Scripts/ProfessorMove.cs
+++ Assets/Scripts/ProfessorMove.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class ProfessorMove : MonoBehaviour
 {
     public Animator anim;
-    
+    //public Rigidbody rbody;
+    float forwardTime = 0;
+
+    private int currentFrameCount = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        //anim.transform.
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        anim.SetFloat("forwardSpeed", 1f);
+    }
+
+    private void OnCollisionEnter(Collision collision)
     {
         
     }
