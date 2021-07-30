d3787c788d4a6f609eb0239e284d113d2ddebc08
Liam Fernandez <liamdfernandez@gmail.com>
2021-07-29 18:50:01 -0700

    Made it so that the alphalevel has updated tutorial aspects

---
+++ Assets/Scripts/StartGravity.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGravity : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            this.GetComponent<Rigidbody>().useGravity = true;
+            this.GetComponent<Rigidbody>().isKinematic = false;
+        }
+    }
+}
---
+++ Assets/Scripts/StartTutorialAI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartTutorialAI : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player")) {
+            GameObject enemy = GameObject.Find("TutorialOpponent");
+            PurePursuitAi script = enemy.GetComponent<PurePursuitAi>();
+            script.startChase = true;
+        }
+    }
+}
